ff7b0ba94cccd7c5daa2cf3e38b680431fe5af45
Pier <pierfrancesco297@gmail.com>
2021-11-02 16:02:12 +0100

    funzioni 3 modalità di gioco

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,61 @@
 /* L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
 in cui ogni cella contiene un numero tra quelli compresi in un range:
-con difficoltà 1 => tra 1 e 100
-con difficoltà 2 => tra 1 e 81
-con difficoltà 3 => tra 1 e 49 */
+con difficoltà 1 => tra 1 e 100 10 * 10
+con difficoltà 2 => tra 1 e 81 9 * 9
+con difficoltà 3 => tra 1 e 49 7 * 7 
+ */
 
-const gridContainer = document.getElementById("grid")
+// crea tre funzioni con cicli for in base al livello scelto dall'utente.
 
-// crea tre diverse funzioni in base al livello scelto dall'utente.
-
-// ogni funzione contiene un ciclo for che imposta alla griglia il numero di caselle associato al livello.
+// ogni ciclo imposta alla griglia il numero di caselle associato al livello.
 
 // con if/else if/else collegare i bottoni ed eseguire la funzione corrispondente al livello scelto.
+
+const gridContainer = document.getElementById("grid");
+const gamemModeEasy = document.getElementById("easy-btn");
+const gamemModeMedium = document.getElementById("medium-btn");
+const gamemModeHard = document.getElementById("hard-btn");
+
+// dichiarazioni funzioni
+let firstOption = easyButton;
+let secondOption = middleButton;
+let thirdOption = hardButton;
+
+
+
+// primo ciclo modalità facile
+ function easyButton() {
+
+    for(let i = 0; i < 100; i++){
+    let node = document.createElement("div");
+    node.classList.add("square");
+
+    gridContainer.appendChild(node);
+
+    }
+}
+
+// secondo ciclo modalità media
+function middleButton() {
+
+    for(let i = 0; i < 81; i++){
+    let node = document.createElement("div");
+    node.classList.add("square");
+
+    gridContainer.appendChild(node);
+
+    }
+}
+
+
+// terzo ciclo modalità difficile
+function hardButton() {
+
+    for(let i = 0; i < 49; i++){
+    let node = document.createElement("div");
+    node.classList.add("square");
+
+    gridContainer.appendChild(node);
+
+    }
+}
