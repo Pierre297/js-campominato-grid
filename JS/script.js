77bd03644e71b2093b7617808aa828b37cf8f7b2
Pier <pierfrancesco297@gmail.com>
2021-11-02 15:28:59 +0100

    Html, css e commenti js

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+/* L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
+in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49 */
+
+const gridContainer = document.getElementById("grid")
+
+// crea tre diverse funzioni in base al livello scelto dall'utente.
+
+// ogni funzione contiene un ciclo for che imposta alla griglia il numero di caselle associato al livello.
+
+// con if/else if/else collegare i bottoni ed eseguire la funzione corrispondente al livello scelto.
